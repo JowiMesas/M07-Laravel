c4d1d586a2efe7a88ef861c6f94393a0a0e74489
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-01-17 18:50:59 +0100

    Count films #9 #4

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('year')->group(function() {
         Route::get('filmsByYear/{year}',[FilmController::class, "listFilmsByYear"])->name('filmsByYear');
         Route::get('filmsByGenre/{genre}',[FilmController::class, 'listFilmsByGenre'])->name('filmsByGenre');
         Route::get('sortFilms',[FilmController::class, "listSortFilmsByYear"])->name("sortFilms");
+        Route::get('countFilms', [FilmController::class, "countFilms"])->name("countFilms");
     });
 });
 
